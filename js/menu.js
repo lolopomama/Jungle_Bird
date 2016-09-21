67190b4270a1b45976d4b0c8b5a868014aed37cf
tachakarn <lolopomama@hotmail.com>
2016-09-22 00:21:12 +0700

    change a bit of concept and doing more Menu + decide to make sensence moving

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,20 @@ var menuState = {
     create: function(){
         game.add.image(0,0 , 'menuWall');
         
+        var nameLabel = game.add.text(game.width/2, -100, 'Jungle Bird', {font: '40px Arial', fill: '#ffffff' });
+        nameLabel.anchor.setTo(0.5, 0.5);
+        
+        game.add.tween(nameLabel).to({y: 80}, 1500).easing(Phaser.Easing.Bounce.Out).start();
+        
+        
+        //Add Frenzy and wave motion
+        var frenzyLable = game.add.text(game.width/2, game.height-500, 'FRENZY', {font: '30px Arial', fill: '#ffffff' });
+        
+        frenzyLable.anchor.setTo(0.5, 0.5);
+        
+        game.add.tween(frenzyLable).to({angle: -2}, 500).to({angle: 2 }, 1000 ).to({angle:0}, 500).loop().start();
+        
+        
         
     },
         
